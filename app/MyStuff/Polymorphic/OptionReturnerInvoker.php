3c7ebe21c3adf1179092b4c9d7c69da6dceeb13d
Kevin Washington <kevw12188@gmail.com>
2014-11-21 20:01:55 -0500

    added functionality to get all the types of options (roles, industries, contactrelations) and also specific options. All with tests

--- app/MyStuff/Polymorphic/OptionReturnerInvoker.php
+++ app/MyStuff/Polymorphic/OptionReturnerInvoker.php
@@ -12,14 +12,38 @@ namespace App\MyStuff\Polymorphic;
 class OptionReturnerInvoker {
 
     //call getAllRoles on a OptionReturner resource
+    public function getAllRoles(OptionReturner $optionReturner)
+    {
+        return $optionReturner->roles;
+    }
 
     //call getSpecificRole on a OptionReturner resource (argument)
+    public function getSpecificRole(OptionReturner $optionReturner, $key)
+    {
+        return $optionReturner->roles[--$key];
+    }
 
     //call getAllIndustries on a OptionReturner resource
+    public function getAllIndustries(OptionReturner $optionReturner)
+    {
+        return $optionReturner->industries;
+    }
 
     //call getSpecificIndustry on a OptionReturner resource
+    public function getSpecificIndustry(OptionReturner $optionReturner, $key)
+    {
+        return $optionReturner->industries[--$key];
+    }
 
     //call getAllRelations on a OptionReturner resource
+    public function getAllContactRelations(OptionReturner $optionReturner)
+    {
+        return $optionReturner->contactRelations;
+    }
 
     //call getSpecificRelations on a OptionReturner resource
+    public function getSpecificContactRelation(OptionReturner $optionReturner, $key)
+    {
+        return $optionReturner->contactRelations[--$key];
+    }
 }
--- tests/OptionReturner/OptionReturnerInvokerTest.php
+++ tests/OptionReturner/OptionReturnerInvokerTest.php
@@ -9,42 +9,70 @@
 namespace tests\OptionReturner;
 
 
+use App\MyStuff\Polymorphic\OptionReturner;
+use App\MyStuff\Polymorphic\OptionReturnerInvoker;
+
 class OptionReturnerInvokerTest extends \PHPUnit_Framework_TestCase {
 
     //call getAllRoles on a OptionReturner resource
     public function test_OptionReturnerInvoker_getAllRoles_retrieves_all_roles()
     {
+        $optionReturner = new OptionReturner();
+
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals(true, is_array($optionReturnerInvoker->getAllRoles($optionReturner)));
 
     }
 
     //call getSpecificRole on a OptionReturner resource (argument)
     public function test_OptionReturnerInvoker_getSpecificRole_retrieves_a_specific_role()
     {
+        $optionReturner = new OptionReturner();
 
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals('Customer Support', $optionReturnerInvoker->getSpecificRole($optionReturner, 1));
     }
 
     //call getAllIndustries on a OptionReturner resource
     public function test_OptionReturnerInvoker_getAllIndustries_retrieves_all_industries()
     {
+        $optionReturner = new OptionReturner();
 
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals(true, is_array($optionReturnerInvoker->getAllIndustries($optionReturner)));
     }
 
     //call getSpecificIndustry on a OptionReturner resource
     public function test_OptionReturnerInvoker_getSpecificIndustry_retrieves_a_specific_industry()
     {
+        $optionReturner = new OptionReturner();
 
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals('Agriculture', $optionReturnerInvoker->getSpecificIndustry($optionReturner, 1));
     }
 
     //call getAllRelations on a OptionReturner resource
     public function test_OptionReturnerInvoker_getAllRelations_retrieves_all_relations()
     {
+        $optionReturner = new OptionReturner();
 
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals(true, is_array($optionReturnerInvoker->getAllContactRelations($optionReturner)));
     }
 
     //call getSpecificRelations on a OptionReturner resource
     public function test_OptionReturnerInvoker_getSpecificRelation_retrieves_a_specific_relation()
     {
+        $optionReturner = new OptionReturner();
 
+        $optionReturnerInvoker = new OptionReturnerInvoker();
+
+        $this->assertEquals('Freelancer',$optionReturnerInvoker->getSpecificContactRelation($optionReturner, 1));
     }
 
 }
