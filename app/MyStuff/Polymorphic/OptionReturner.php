e621747b5f4e6bf722704d0e392694e53d004a6e
Kevin Washington <kevw12188@gmail.com>
2014-11-21 19:21:35 -0500

    added functionality for optionreturner class to store acceptable values for Roles, contactRelations, and Industries in the application with tests

--- app/MyStuff/Polymorphic/OptionReturner.php
+++ app/MyStuff/Polymorphic/OptionReturner.php
@@ -11,4 +11,73 @@ namespace App\MyStuff\Polymorphic;
 
 class OptionReturner {
 
+
+    /**
+     * The values acceptable for Roles in application
+     * @var array
+     */
+    public $roles = [
+        //Customer Support must be the first value or things will break!
+        'Customer Support',
+
+        'Admin Support',
+
+        'Marketing',
+
+        'Sales',
+
+        'Developer',
+
+        'Designer',
+
+        'IT',
+
+        'Management'
+
+    ];
+
+
+    /**
+     * The values acceptable for Industries in application
+     * @var array
+     */
+    public $industries = [
+        //Agriculture must be first or things will break!
+        'Agriculture',
+
+        'Accounting',
+
+        'Advertising',
+
+        'Aerospace',
+
+        'Aircraft',
+
+        'Airline',
+
+        'Apparel & Accessories',
+
+    ];
+
+
+    /**
+     * The values acceptable for Contact Relations in application
+     * @var array
+     */
+    public $contactRelations = [
+        //Freelance must be first or things will break!
+        'Freelancer',
+
+        'Consultant',
+
+        'Employee',
+
+        'Vendor',
+
+        'Investor',
+
+        'Partner',
+
+    ];
+
 }
---
+++ tests/OptionReturner/OptionReturnerTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/21/14
+ * Time: 7:08 PM
+ */
+
+namespace tests\OptionReturner;
+
+
+use App\MyStuff\Polymorphic\OptionReturner;
+
+class OptionReturnerTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_optionReturner_stores_roles_as_a_property()
+    {
+        $OptionReturner = new OptionReturner();
+
+        $this->assertEquals(true, is_array($OptionReturner->roles));
+    }
+
+    public function test_optionReturner_stores_industries_as_a_property()
+    {
+        $OptionReturner = new OptionReturner();
+
+        $this->assertEquals(true, is_array($OptionReturner->industries));
+    }
+
+    public function test_optionReturner_stores_relations_as_a_property()
+    {
+        $OptionReturner = new OptionReturner();
+
+        $this->assertEquals(true, is_array($OptionReturner->contactRelations));
+    }
+
+    public function test_optionReturner_has_correct_values_stored_first()
+    {
+        $OptionReturner = new OptionReturner();
+        $this->assertEquals('Customer Support', $OptionReturner->roles[0]);
+        $this->assertEquals('Agriculture', $OptionReturner->industries[0]);
+        $this->assertEquals('Freelancer', $OptionReturner->contactRelations[0]);
+    }
+
+}
