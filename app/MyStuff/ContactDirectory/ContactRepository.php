7ba198e9fcc506eb258c6b31574117fc2a1cc2d7
Kevin Washington <kevw12188@gmail.com>
2014-11-25 11:50:02 -0500

    created all files for contact resource

---
+++ app/MyStuff/ContactDirectory/ContactRepository.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/25/14
+ * Time: 11:47 AM
+ */
+
+namespace App\MyStuff\ContactDirectory;
+
+
+class ContactRepository {
+
+}
---
+++ app/MyStuff/ContactDirectory/ContactValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/25/14
+ * Time: 11:46 AM
+ */
+
+namespace App\MyStuff\ContactDirectory;
+
+
+class ContactValidator {
+
+}
