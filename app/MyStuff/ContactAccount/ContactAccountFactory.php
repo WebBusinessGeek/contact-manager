2ac91b5844d7c384ee8c93d70fed28fc38238526
Kevin Washington <kevw12188@gmail.com>
2014-11-30 18:57:31 -0500

    added files to begin developing the contactAccount domain level

---
+++ app/MyStuff/ContactAccount/ContactAccountFactory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/30/14
+ * Time: 6:56 PM
+ */
+
+namespace App\MyStuff\ContactAccount;
+
+
+class ContactAccountFactory {
+
+}
---
+++ app/MyStuff/ContactAccount/ContactAccountResponder.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 11/30/14
+ * Time: 6:55 PM
+ */
+
+namespace App\MyStuff\ContactAccount;
+
+
+class ContactAccountResponder {
+
+}
