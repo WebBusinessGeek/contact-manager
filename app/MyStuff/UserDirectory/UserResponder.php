1e14e87eb658839d7cd47105ebdd64b30130c068
Kevin Washington <kevw12188@gmail.com>
2014-12-03 22:41:32 -0500

    added classes and files to begin developing the user domain logic

---
+++ app/MyStuff/UserDirectory/UserResponder.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 12/3/14
+ * Time: 10:37 PM
+ */
+
+namespace App\MyStuff\UserDirectory;
+
+
+class UserResponder {
+
+}
---
+++ app/MyStuff/UserDirectory/UserValidator.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 12/3/14
+ * Time: 10:36 PM
+ */
+
+namespace App\MyStuff\UserDirectory;
+
+
+class UserValidator {
+
+}
